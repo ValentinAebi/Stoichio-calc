d5e40ca930bcf58ba413fedb1846b12e09951b50
Valentin Aebi <valentin.aebi@epfl.ch>
2022-07-06 11:29:01 +0200

    Module for test atoms + res folder

--- tests/parsing_tests.rs
+++ tests/parsing_tests.rs
@@ -1,5 +1,4 @@
 mod parsing_tests {
-    use Stoichio_calc::chemistry::Atom;
     use Stoichio_calc::parsing::{parse_atom, Token, tokenize, TokenType};
     use Stoichio_calc::parsing::TokenType::{Alphabetic, Numeric};
 
@@ -30,46 +29,74 @@ mod parsing_tests {
 
     #[test]
     fn parse_atom_test() {
-        let hydrogen: Atom = Atom {
-            name: "hydrogen".to_string(),
-            code: "H".to_string(),
-            atomic_mass_milli_uma: 1_007,
-        };
-        let carbon: Atom = Atom {
-            name: "carbon".to_string(),
-            code: "C".to_string(),
-            atomic_mass_milli_uma: 12_011,
-        };
-        let oxygen: Atom = Atom {
-            name: "oxygen".to_string(),
-            code: "O".to_string(),
-            atomic_mass_milli_uma: 15_999,
-        };
-        let nitrogen: Atom = Atom {
-            name: "nitrogen".to_string(),
-            code: "N".to_string(),
-            atomic_mass_milli_uma: 14_007,
-        };
-        let sodium: Atom = Atom {
-            name: "sodium".to_string(),
-            code: "Na".to_string(),
-            atomic_mass_milli_uma: 22_990,
-        };
-        let atoms = Vec::from([
-            &hydrogen, &carbon, &oxygen, &nitrogen, &sodium
-        ]).iter().map(
-            |&atom| { (atom.code.clone(), atom.clone()) }
-        ).collect();
-
         let molec_str = "H2O".to_string();
         let mut tokens = tokenize(&molec_str);
-        let act = parse_atom(atoms, &mut tokens);
+        let act = parse_atom(test_atoms::atoms_map(), &mut tokens);
         assert_eq!(Vec::from([
             Token("2".to_string(), Numeric),
             Token("O".to_string(), Alphabetic)
         ]), tokens);
-        assert_eq!(Some(hydrogen), act)
+        assert_eq!(Some(test_atoms::hydrogen()), act)
     }
+
+    mod test_atoms {
+        use std::collections::BTreeMap;
+        use Stoichio_calc::chemistry::Atom;
+
+        pub fn hydrogen() -> Atom {
+            Atom {
+                name: "hydrogen".to_string(),
+                code: "H".to_string(),
+                atomic_mass_milli_uma: 1_007,
+            }
+        }
+
+        pub fn carbon() -> Atom {
+            Atom {
+                name: "carbon".to_string(),
+                code: "C".to_string(),
+                atomic_mass_milli_uma: 12_011,
+            }
+        }
+
+        pub fn oxygen() -> Atom {
+            Atom {
+                name: "oxygen".to_string(),
+                code: "O".to_string(),
+                atomic_mass_milli_uma: 15_999,
+            }
+        }
+
+        pub fn nitrogen() -> Atom {
+            Atom {
+                name: "nitrogen".to_string(),
+                code: "N".to_string(),
+                atomic_mass_milli_uma: 14_007,
+            }
+        }
+
+        pub fn sodium() -> Atom {
+            Atom {
+                name: "sodium".to_string(),
+                code: "Na".to_string(),
+                atomic_mass_milli_uma: 22_990,
+            }
+        }
+
+        pub fn all_atoms() -> Vec<Atom> {
+            Vec::from([
+                hydrogen(), carbon(), oxygen(), nitrogen(), sodium()
+            ])
+        }
+
+        pub fn atoms_map() -> BTreeMap<String, Atom> {
+            all_atoms().iter().map(
+                |atom| { (atom.code.clone(), atom.clone()) }
+            ).collect()
+        }
+
+    }
+
 }
 
 
